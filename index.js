e29fdf828f0af5d3c96eb9232bd71ffccfe65234
Prashant Kumar Maurya <45981671+prashant4471@users.noreply.github.com>
2023-12-09 20:23:39 +0530

    body-parser added

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const PORT = 3000;
 app.listen(PORT,'127.0.0.1',() => {
     console.log('Server is listening on 127.0.0.1:'+PORT);
